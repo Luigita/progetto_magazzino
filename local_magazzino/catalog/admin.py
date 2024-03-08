7b02aadbc5ae3573d5b8c680b4a343d8f651129d
Luigi <gigitanzillo@gmail.com>
2024-03-08 18:32:31 +0100

    skeleton

--- local_magazzino/catalog/admin.py
+++ local_magazzino/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Materiale, Movimenti
+
+admin.site.register(Materiale)
+admin.site.register(Movimenti)
+
+# from .models import Author, Genre, Book, BookInstance
+# # Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Genre)
+# admin.site.register(BookInstance)
--- test/test.py
+++ test/test.py
@@ -12,5 +12,17 @@ class Materiale(models.Model):
 	def __str__(self):
 		return self.descrizione
 
-	# def get_absolute_url(self):
-	# 	return reverse()
+
+# def get_absolute_url(self):
+# 	return reverse()
+
+
+class Movimenti(models.Model):
+	"""Modello rappresentante la movimentazione di un materiale"""
+	materiale = Materiale
+	quantita = models.IntegerField()
+	magazzino = models.CharField(max_length="3")
+	data_movimento = models.DateTimeField()
+
+	def __str__(self):
+		return self.materiale
